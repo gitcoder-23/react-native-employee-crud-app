93cfc6c601965341044309b83e866becb6e6a3c8
gitcoder-23 <prabhatece21@gmail.com>
2022-02-02 11:48:31 +0530

    picture added

--- screens/Profile.js
+++ screens/Profile.js
@@ -10,12 +10,19 @@ const Profile = () => {
           colors={['#0033ff', '#6bc1ff']}
           style={{ height: '20%' }}
         />
-        <Image
-          style={{ width: 100, height: 100, borderRadius: 100 / 2 }}
-          source={{
-            uri: 'https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-          }}
-        />
+        <View style={{ alignItems: 'center' }}>
+          <Image
+            style={{
+              width: 140,
+              height: 140,
+              borderRadius: 140 / 2,
+              marginTop: -50,
+            }}
+            source={{
+              uri: 'https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+            }}
+          />
+        </View>
       </View>
     </>
   );
