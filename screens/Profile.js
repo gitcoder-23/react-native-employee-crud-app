3de53d61cc20188fb5ada380f1fd9e2f54482414
gitcoder-23 <prabhatece21@gmail.com>
2022-02-02 11:45:00 +0530

    Image added

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -9,6 +9,12 @@ const Profile = () => {
         <LinearGradient
           colors={['#0033ff', '#6bc1ff']}
           style={{ height: '20%' }}
+        />
+        <Image
+          style={{ width: 100, height: 100, borderRadius: 100 / 2 }}
+          source={{
+            uri: 'https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+          }}
         />
       </View>
     </>
